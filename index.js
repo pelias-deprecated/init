67786409b6dc0c5e94c3075cd3e8bde8e553ddd0
Severyn Kozak <severyn.kozak@gmail.com>
2015-01-29 13:17:48 -0500

    Bundle prompt initialization into a function.
    index.js
    	-Bundle all module-level `prompt` initialization/configuration
    	code into an `initPrompt()` function.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * @file i
+ * @file
  */
 
 'use strict';
@@ -10,43 +10,45 @@ var path = require( 'path' );
 var childProcess = require( 'child_process' );
 var markup = require( 'markup-js' );
 
-prompt.start();
+(function initPrompt(){
+  prompt.start();
 
-var schema = {
-  properties: {
-    name: {
-      pattern: /^[a-z0-9._-]*$/i,
-      message: 'Valid name characters: [a-zA-Z0-9._-]',
-      required: true
-    },
-    description: {},
-    keywords: {
-      description: 'Comma-separated list of keywords'
-    },
-    tests: {
-      description: 'Initialize unit-tests? [yn]',
-      pattern: /^[yn]$/i
+  var schema = {
+    properties: {
+      name: {
+        pattern: /^[a-z0-9._-]*$/i,
+        message: 'Valid name characters: [a-zA-Z0-9._-]',
+        required: true
+      },
+      description: {},
+      keywords: {
+        description: 'Comma-separated list of keywords'
+      },
+      tests: {
+        description: 'Initialize unit-tests? [yn]',
+        pattern: /^[yn]$/i
+      }
     }
-  }
-};
+  };
 
-prompt.get(
-  schema,
-  function ( err, input ){
-    if( err ){
-      console.error( err );
-      process.exit( 1 );
+  prompt.get(
+    schema,
+    function ( err, input ){
+      if( err ){
+        console.error( err );
+        process.exit( 1 );
+      }
+      initializeProject(
+        input.name,
+        input.description,
+        input.keywords.split( ',' ).map( function trim( keyword ){
+          return keyword.trim();
+        }),
+        input.tests.toLowerCase() === 'y'
+      );
     }
-    initializeProject(
-      input.name,
-      input.description,
-      input.keywords.split( ',' ).map( function trim( keyword ){
-        return keyword.trim();
-      }),
-      input.tests.toLowerCase() === 'y'
-    );
-  }
-);
+  );
+})();
 
 function getTemplateFilePath( filePath ){
   return path.join( __dirname, 'project_template', filePath );
@@ -82,12 +84,20 @@ function initializeProject( name, description, keywords, tests ){
     copyTemplateFile( '.travis.yml' );
   }
 
+  /**
+   * Formatting the `keywords` value is easier/cleaner here than it is in the
+   * Markup template.
+   */
+  var formattedKeywords = keywords.map( function enquote( str ){
+    return ( str.length > 0) ?
+      '"' + str + '"' :
+      str;
+  }).join( ', ' );
+
   var strPkgJson = readFormatFileSync( 'package.json', {
     name: name,
     description: description,
-    keywords: keywords.map( function quote( str ){
-      return '"' + str + '"';
-    }),
+    keywords: formattedKeywords,
     tests: tests
   });
   fs.writeFileSync( 'package.json', strPkgJson );
